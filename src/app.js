7bbef53456f04dce343ebed1c227b1f5044470c0
knaevery <111208041+knaevery@users.noreply.github.com>
2022-09-21 18:43:00 +1000

    Added data description of weather

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
+  let descriptionElement = document.querySelector("#description");
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "223a4421e9dbb0fc1bd3e8fae6ad0b9e";
