3753d2fe9195e0e1c2aab33afa2b8e9ce245e8c1
knaevery <111208041+knaevery@users.noreply.github.com>
2022-09-21 12:59:53 +1000

    added openweatherapp
    Not working though

---
+++ src/app.js
@@ -0,0 +1,4 @@
+let apiKey = "223a4421e9dbb0fc1bd3e8fae6ad0b9e";
+let cityName = "sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+console.log(apiUrl);
