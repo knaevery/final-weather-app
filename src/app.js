f8b301e3b52b2e6953003c861cdd48ff2069499d
knaevery <111208041+knaevery@users.noreply.github.com>
2022-09-21 19:43:42 +1000

    Testing different city names

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "223a4421e9dbb0fc1bd3e8fae6ad0b9e";
-let cityName = "sydney";
+let cityName = "chiang mai";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
