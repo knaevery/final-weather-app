35c26f7227a66e9088c1afc87dfec84b0a6032ff
knaevery <111208041+knaevery@users.noreply.github.com>
2022-09-21 19:39:56 +1000

    Added data for day, date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let descriptionElement = document.querySelector("#description");
@@ -5,12 +28,14 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeAndDateElement = document.querySelector("#timeAndDate");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  timeAndDateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "223a4421e9dbb0fc1bd3e8fae6ad0b9e";
