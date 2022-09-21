688c52fb35634b58fe44c6a984370cdabbaf137d
knaevery <111208041+knaevery@users.noreply.github.com>
2022-09-21 13:56:50 +1000

    Testing Axios and OpenWeatherApp

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
 let apiKey = "223a4421e9dbb0fc1bd3e8fae6ad0b9e";
 let cityName = "sydney";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-console.log(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
