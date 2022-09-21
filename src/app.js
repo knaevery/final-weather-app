aec48c67ec35c2c68a61f8ca31d68a72289fdade
knaevery <111208041+knaevery@users.noreply.github.com>
2022-09-21 14:19:29 +1000

    Adding javascript
    for responsive temperature and city names

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 function displayTemperature(response) {
   console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
 }
+
 let apiKey = "223a4421e9dbb0fc1bd3e8fae6ad0b9e";
 let cityName = "sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
